0bb8c1e1a52ed87d89f01a5a8adcba539bc8ef78
Bela <bela.schinke@gmail.com>
2023-12-06 22:17:29 +0900

    changed connection confing to allow wss

--- src/main/java/com/belaschinke/webgamebackend/config/WebSocketConfig.java
+++ src/main/java/com/belaschinke/webgamebackend/config/WebSocketConfig.java
@@ -4,7 +4,9 @@ import com.belaschinke.webgamebackend.service.GameMessageHandler;
 import com.belaschinke.webgamebackend.service.tocTacToe.TicTacToeGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -21,6 +23,12 @@ public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBro
                 .setAllowedOrigins("*");
     }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes( "/app");
+    }
+
     @Bean
     public GameMessageHandler<TicTacToeGame> ticTacToeHandler() {
         return new GameMessageHandler();
@@ -29,5 +37,12 @@ public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBro
     @Bean
     public Class<TicTacToeGame> ticTacToeGameClass() {
         return TicTacToeGame.class;
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(16384);
+        return container;
     }
 }
