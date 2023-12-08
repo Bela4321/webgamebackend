e865835fbd16cc06b3c7bb9c9021204a2fa1ad7a
Bela <bela.schinke@gmail.com>
2023-12-08 18:26:25 +0900

    made work with https

--- src/main/java/com/belaschinke/webgamebackend/config/WebSocketConfig.java
+++ src/main/java/com/belaschinke/webgamebackend/config/WebSocketConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -39,10 +38,5 @@ public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBro
         return TicTacToeGame.class;
     }
 
-    @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxBinaryMessageBufferSize(16384);
-        return container;
-    }
+
 }
