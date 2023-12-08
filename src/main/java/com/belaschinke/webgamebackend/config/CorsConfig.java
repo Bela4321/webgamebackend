ead6011b76889a5d1dfe8101b8ee94497acf2c66
Bela <bela.schinke@gmail.com>
2023-12-08 19:06:46 +0900

    added cors config

---
+++ src/main/java/com/belaschinke/webgamebackend/config/CorsConfig.java
@@ -0,0 +1,12 @@
+package com.belaschinke.webgamebackend.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/ticTacToe").allowedOrigins("*");
+    }
+}
